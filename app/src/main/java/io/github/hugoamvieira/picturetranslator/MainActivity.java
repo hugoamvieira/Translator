2084bdf501da9c924b7332e788c213b11f0202de
Hugo Vieira <hugo.andre.vieira@gmail.com>
2016-11-20 23:39:32 +0000

    Added new activity for translation history

--- app/src/main/java/io/github/hugoamvieira/picturetranslator/MainActivity.java
+++ app/src/main/java/io/github/hugoamvieira/picturetranslator/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.afollestad.materialcamera.MaterialCamera;
@@ -21,9 +23,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Start camera in stillshot mode
-        final MaterialCamera camera = new MaterialCamera(MainActivity.this);
-        camera.stillShot().start(CAMERA_RQ);
+        // Get UI elements
+        Button takePictureBtn = (Button) findViewById(R.id.take_picture_btn);
+        Button translationHistBtn = (Button) findViewById(R.id.translation_history_btn);
+
+        // Set OnClickListeners for buttons
+        takePictureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start camera in stillshot mode
+                final MaterialCamera camera = new MaterialCamera(MainActivity.this);
+                camera.stillShot().start(CAMERA_RQ);
+            }
+        });
+
+        translationHistBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, HistoryActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
